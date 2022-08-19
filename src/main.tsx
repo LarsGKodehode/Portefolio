ce0854ee2ce5977d0fba1e00a3d3944a4d540ef7
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-19 14:11:27 +0200

    added some dev simplifying

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,20 @@ import Projects from './pages/projects/Projects';
 import Contact from './pages/contact/Contact';
 
 
+
+/**
+ * DEVELOPMENT
+ * clears console of error messages between hotswaps
+ * @link https://github.com/vitejs/vite/discussions/3143
+ */ 
+ if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
+// DEVELOPMENT
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
