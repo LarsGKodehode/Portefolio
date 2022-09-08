ccbc9dd8cb541c5fbf98b9278b0e11d8104907e4
Lars G <104063134+LarsGKodehode@users.noreply.github.com>
2022-09-08 14:02:43 +0200

    hotfix, compile-fail (#32)
    * added test build script
    
    * commented out faulty code
    
    it's unused at the moment and causes compilation to fail. I will revisit this concept at a later date.

--- src/utilities/utils/utils.ts
+++ src/utilities/utils/utils.ts
@@ -7,33 +7,33 @@ import { Dimensions } from './utils.d';
  * @param container React Refrence
  * @link stackoverflow: https://stackoverflow.com/a/60978633
  */
-export const useContainerDimensions = (
-  container: React.RefObject<HTMLElement>
-  ):
-  any
-  | null => {
-  // Get dimensions of container
-  const getDimensions = (): Dimensions => {
-    return {
-      width: container.current.offsetWidth,
-      height: container.current.offsetHeight,
-    };
-  };
+// export const useContainerDimensions = (
+//   container: React.RefObject<HTMLElement>
+//   ):
+//   any
+//   | null => {
+//   // Get dimensions of container
+//   const getDimensions = (): Dimensions => {
+//     return {
+//       width: container.current.offsetWidth,
+//       height: container.current.offsetHeight,
+//     };
+//   };
 
-  // Store them in state
-  const [dimensions, setDimensions] = React.useState<Dimensions>({width: 0, height: 0});
+//   // Store them in state
+//   const [dimensions, setDimensions] = React.useState<Dimensions>({width: 0, height: 0});
 
-  // Handle resize event
-  useEffect(() => {
-    // Set new dimensions
-    const handleResize = () => {
-      setDimensions(getDimensions());
-    };
+//   // Handle resize event
+//   useEffect(() => {
+//     // Set new dimensions
+//     const handleResize = () => {
+//       setDimensions(getDimensions());
+//     };
 
-    // Check if !null
-    if(container.current !== null) {
-      setDimensions(getDimensions());
-    };
+//     // Check if !null
+//     if(container.current !== null) {
+//       setDimensions(getDimensions());
+//     };
     
-  }, [container]);
-};
+//   }, [container]);
+// };
