93085af0b2a99be52d5186de4641128528486dfc
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-09-11 18:59:20 +0200

    added hardcoded info about projects.
    this was done to get some progression done, while I figure out how to use GitHubs GraphQL.

--- src/pages/projects/data.projects.ts
+++ src/pages/projects/data.projects.ts
@@ -1,5 +1,237 @@
 const DataProjects = {
-
-};
+  "repositoryOwner": {
+    "name": "Lars G",
+    "bio": "Student Developer @Jobloop",
+    "repositories": {
+      "totalCount": 32,
+      "nodes": [
+        {
+          "id": "R_kgDOHPLU2g",
+          "name": "LarsGKodehode",
+          "description": "Config files for my GitHub profile.",
+          "updatedAt": "2022-04-26T07:20:17Z",
+          "url": "https://github.com/LarsGKodehode/LarsGKodehode"
+        },
+        {
+          "id": "R_kgDOHPO4RQ",
+          "name": "pre-homepage",
+          "description": "pre-release for hompage",
+          "updatedAt": "2022-04-26T10:59:33Z",
+          "url": "https://github.com/LarsGKodehode/pre-homepage"
+        },
+        {
+          "id": "R_kgDOHP_Rrw",
+          "name": "LarsGKodehode.github.io",
+          "description": "used for Github Pages to act as server for web pages ",
+          "updatedAt": "2022-04-28T10:15:43Z",
+          "url": "https://github.com/LarsGKodehode/LarsGKodehode.github.io"
+        },
+        {
+          "id": "R_kgDOHlzsiA",
+          "name": "algorithms",
+          "description": "A collection of algorithm implementation that I found fun, challenging or useful ",
+          "updatedAt": "2022-07-05T16:47:34Z",
+          "url": "https://github.com/LarsGKodehode/algorithms"
+        },
+        {
+          "id": "R_kgDOHmnqQQ",
+          "name": "knowCation",
+          "description": null,
+          "updatedAt": "2022-07-08T08:32:30Z",
+          "url": "https://github.com/LarsGKodehode/knowCation"
+        },
+        {
+          "id": "R_kgDOHqZaYQ",
+          "name": "chrome-dashoard",
+          "description": null,
+          "updatedAt": "2022-07-15T09:54:09Z",
+          "url": "https://github.com/LarsGKodehode/chrome-dashoard"
+        },
+        {
+          "id": "R_kgDOHrMgTw",
+          "name": "react-first-test",
+          "description": null,
+          "updatedAt": "2022-07-18T12:38:35Z",
+          "url": "https://github.com/LarsGKodehode/react-first-test"
+        },
+        {
+          "id": "R_kgDOHrqChg",
+          "name": "Air-BnB-Copycat",
+          "description": null,
+          "updatedAt": "2022-07-20T08:20:39Z",
+          "url": "https://github.com/LarsGKodehode/Air-BnB-Copycat"
+        },
+        {
+          "id": "R_kgDOHsBQbg",
+          "name": "meme-generator",
+          "description": null,
+          "updatedAt": "2022-07-22T08:02:13Z",
+          "url": "https://github.com/LarsGKodehode/meme-generator"
+        },
+        {
+          "id": "R_kgDOHuxAZQ",
+          "name": "docker-first-stab",
+          "description": "First try at setting up a local server using Docker containers",
+          "updatedAt": "2022-07-28T10:35:28Z",
+          "url": "https://github.com/LarsGKodehode/docker-first-stab"
+        },
+        {
+          "id": "R_kgDOHwBUxA",
+          "name": "docker-second-stab",
+          "description": "Second implementation of a simple NodeJS serve, looking into REST API and sniffing at Mocha as a test framework.",
+          "updatedAt": "2022-08-01T13:14:58Z",
+          "url": "https://github.com/LarsGKodehode/docker-second-stab"
+        },
+        {
+          "id": "R_kgDOHwSaZQ",
+          "name": "git-workflows",
+          "description": "Collection of useful git workflows",
+          "updatedAt": "2022-08-02T07:25:22Z",
+          "url": "https://github.com/LarsGKodehode/git-workflows"
+        },
+        {
+          "id": "R_kgDOHwUgsQ",
+          "name": "stock-chart",
+          "description": "A simple app using React to display stock data.",
+          "updatedAt": "2022-08-16T13:18:46Z",
+          "url": "https://github.com/LarsGKodehode/stock-chart"
+        },
+        {
+          "id": "R_kgDOHwYH5Q",
+          "name": "js-quirks",
+          "description": null,
+          "updatedAt": "2022-08-02T12:57:13Z",
+          "url": "https://github.com/LarsGKodehode/js-quirks"
+        },
+        {
+          "id": "R_kgDOHwqoJw",
+          "name": "quotes",
+          "description": "A categorized collection of various quotes",
+          "updatedAt": "2022-08-03T08:09:12Z",
+          "url": "https://github.com/LarsGKodehode/quotes"
+        },
+        {
+          "id": "R_kgDOHwvzRw",
+          "name": "tenzies",
+          "description": "Tenzies project from Scrimba",
+          "updatedAt": "2022-08-03T15:35:24Z",
+          "url": "https://github.com/LarsGKodehode/tenzies"
+        },
+        {
+          "id": "R_kgDOHyPk7A",
+          "name": "react-router-test",
+          "description": null,
+          "updatedAt": "2022-08-08T13:43:35Z",
+          "url": "https://github.com/LarsGKodehode/react-router-test"
+        },
+        {
+          "id": "R_kgDOHyr5-Q",
+          "name": "test-go",
+          "description": "Simple test of Golang",
+          "updatedAt": "2022-08-09T10:55:22Z",
+          "url": "https://github.com/LarsGKodehode/test-go"
+        },
+        {
+          "id": "R_kgDOHzF_WQ",
+          "name": "feed-reader",
+          "description": null,
+          "updatedAt": "2022-08-10T14:27:37Z",
+          "url": "https://github.com/LarsGKodehode/feed-reader"
+        },
+        {
+          "id": "R_kgDOH0k_6w",
+          "name": "styled-components",
+          "description": "Task to show utilization of Styled Components",
+          "updatedAt": "2022-08-15T14:23:55Z",
+          "url": "https://github.com/LarsGKodehode/styled-components"
+        },
+        {
+          "id": "R_kgDOH09FCg",
+          "name": "samarbeid-test",
+          "description": "Enkelt repo for testing av samarbeid",
+          "updatedAt": "2022-08-16T08:19:18Z",
+          "url": "https://github.com/LarsGKodehode/samarbeid-test"
+        },
+        {
+          "id": "R_kgDOH2HhQw",
+          "name": "react-app-test",
+          "description": null,
+          "updatedAt": "2022-08-19T08:02:15Z",
+          "url": "https://github.com/LarsGKodehode/react-app-test"
+        },
+        {
+          "id": "R_kgDOH2JHtw",
+          "name": "react-app-test-2",
+          "description": "testing create-react-app with typescript",
+          "updatedAt": "2022-08-19T09:00:32Z",
+          "url": "https://github.com/LarsGKodehode/react-app-test-2"
+        },
+        {
+          "id": "R_kgDOH2Mutw",
+          "name": "Portefolio",
+          "description": "Personal portefolio",
+          "updatedAt": "2022-08-19T12:13:55Z",
+          "url": "https://github.com/LarsGKodehode/Portefolio"
+        },
+        {
+          "id": "R_kgDOH4T5Tg",
+          "name": "SimpleCounter",
+          "description": "Simple Counter using React useState and some custom components",
+          "updatedAt": "2022-08-25T10:52:03Z",
+          "url": "https://github.com/LarsGKodehode/SimpleCounter"
+        },
+        {
+          "id": "R_kgDOH4ZjkQ",
+          "name": "react-api-call",
+          "description": "Done for an assignment showcasing ",
+          "updatedAt": "2022-08-25T15:04:22Z",
+          "url": "https://github.com/LarsGKodehode/react-api-call"
+        },
+        {
+          "id": "R_kgDOH5r9rg",
+          "name": "useContext",
+          "description": "Showcase of React useContext for assignment",
+          "updatedAt": "2022-08-29T14:15:46Z",
+          "url": "https://github.com/LarsGKodehode/useContext"
+        },
+        {
+          "id": "R_kgDOH7NipQ",
+          "name": "big-project",
+          "description": "Graduating project from Kodehode",
+          "updatedAt": "2022-09-02T08:48:26Z",
+          "url": "https://github.com/LarsGKodehode/big-project"
+        },
+        {
+          "id": "R_kgDOH8LusA",
+          "name": "node-first-test",
+          "description": "First test at setting up a local Nodejs server and serving a static page",
+          "updatedAt": "2022-09-05T11:23:45Z",
+          "url": "https://github.com/LarsGKodehode/node-first-test"
+        },
+        {
+          "id": "R_kgDOH8gOuA",
+          "name": "basic-crud-server",
+          "description": "Task: Familirization with a basic CRUD server setup",
+          "updatedAt": "2022-09-06T11:31:25Z",
+          "url": "https://github.com/LarsGKodehode/basic-crud-server"
+        },
+        {
+          "id": "R_kgDOH8uBJQ",
+          "name": "node-events",
+          "description": "Task setting up a server and testing out nodejs events",
+          "updatedAt": "2022-09-06T17:23:44Z",
+          "url": "https://github.com/LarsGKodehode/node-events"
+        },
+        {
+          "id": "R_kgDOH9ZpBw",
+          "name": "react_lessons_2.0",
+          "description": "React lessons used in my classes",
+          "updatedAt": "2022-06-17T07:22:09Z",
+          "url": "https://github.com/LarsGKodehode/react_lessons_2.0"
+        }
+      ]
+    }
+  }
+}
 
 export default DataProjects;
